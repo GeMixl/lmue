270b3bed6898bec19be421b4c12873a186b8d9ec
GeMixl <g.michl@gmx.net>
2021-03-28 21:32:00 +0200

    half-way changing gui to pyqt5

--- lmueApp.py
+++ lmueApp.py
@@ -1,11 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QFormLayout, QLineEdit, QTableWidget, QFileDialog
+from PyQt5.QtGui import QDoubleValidator, QFont, QIcon
+from PyQt5.QtCore import Qt
 import tkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 import math
 
 LOAD_GRID_SIZE = 10
 
 class App:
+    '''
+    +------------------------master+frame------------------------------+
+    | +---------drawing+frame------------+  +-------load+frame------+  |
+    | |                                  |  |                       |  |
+    | |                                  |  |                       |  |
+    | |                                  |  |                       |  |
+    | +----------------------------------+  +-----------------------+  |
+    |                                                                  |
+    | +----------track+frame-------------+  +-----obstacle+frame----+  |
+    | |                                  |  |                       |  |
+    | |                                  |  |                       |  |
+    | +----------------------------------+  |                       |  |
+    |                                       |                       |  |
+    | +----------control+frame-----------+  |                       |  |
+    | |                                  |  |                       |  |
+    | |                                  |  |                       |  |
+    | +----------------------------------+  +-----------------------+  |
+    +------------------------------------------------------------------+
+    '''
+
     def __init__(self, root):
         # Create a container
         self.main_frame = tkinter.Frame(root)
@@ -94,6 +119,78 @@ class App:
         self.line, = self.ax.plot(self.load_coo['x'], self.load_coo['y'])
         self.canvas.draw()
 
-root = tkinter.Tk()
-app = App(root)
-root.mainloop()
+#root = tkinter.Tk()
+#app = App(root)
+#root.mainloop()
+
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=12, height=12, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My PyQt5 App")
+        #self.setGeometry(100,100)
+        self.layout = QVBoxLayout()
+        self.upperLayout = QHBoxLayout()
+        self.lowerLayout = QHBoxLayout()
+        self.optionsBoxLayout = QFormLayout()
+        self.trackBoxLayout = QFormLayout()
+        self.loadBoxLayout = QVBoxLayout()
+        self.obstacleBoxLayout = QVBoxLayout()
+
+        self.drawingWidget = MplCanvas()
+        self.drawingWidget.axes.plot([1,-1],[5,5])
+        self.upperLayout.addWidget(self.drawingWidget)
+        self.upperLayout.addLayout(self.optionsBoxLayout)
+        self.lowerLayout.addLayout(self.trackBoxLayout)
+        self.lowerLayout.addLayout(self.loadBoxLayout)
+        self.lowerLayout.addLayout(self.obstacleBoxLayout)
+
+        self.layout.addWidget(QLabel("<h3>My PyQt5 App</h3>"))
+        self.layout.addLayout(self.upperLayout)
+        self.layout.addLayout(self.lowerLayout)
+
+        self.radiusInput = QLineEdit()
+        self.radiusInput.setValidator(QDoubleValidator())
+        self.radiusInput.setMaxLength(8)
+        self.radiusInput.setAlignment(Qt.AlignRight)
+        self.cantInput = QLineEdit()
+        self.cantInput.setValidator(QDoubleValidator())
+        self.cantInput.setMaxLength(8)
+        self.cantInput.setAlignment(Qt.AlignRight)
+        self.trackBoxLayout.addWidget(QLabel("Track Settings"))
+        self.trackBoxLayout.addRow("Radius", self.radiusInput)
+        self.trackBoxLayout.addRow("Überhöhung", self.cantInput)
+
+        self.loadTable = QTableWidget()
+        self.loadTable.setRowCount(12)
+        self.loadTable.setColumnCount(2)
+        self.loadBoxLayout.addWidget(self.loadTable)
+
+        self.button = QPushButton("Greet")
+        self.button.resize(100, 100)
+        self.button.setIcon(QIcon("./rotate-icon.png"))
+        self.button.clicked.connect(lambda: self.greeting("Görn"))
+        self.optionsBoxLayout.addWidget(self.button)
+        self.message = QLabel("")
+        self.optionsBoxLayout.addWidget(self.message)
+        self.setLayout(self.layout)
+
+    def greeting(self, text):
+        if self.message.text() == "":
+            self.message.setText("Hello " + text)
+        else:
+            self.message.setText("")
+
+
+app = QApplication([])
+window = MyWindow()
+window.show()
+sys.exit(app.exec_())
